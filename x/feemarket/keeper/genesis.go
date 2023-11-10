039dc35a807568313d319e6b5eb9dbfd4023c678
aljo242 <alex@ingenuity.build>
2023-11-10 12:36:58 -0500

    init

---
+++ x/feemarket/keeper/genesis.go
@@ -0,0 +1,35 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/skip-mev/feemarket/x/feemarket/types"
+)
+
+// InitGenesis initializes the feemarket module's state from a given genesis state.
+func (k *Keeper) InitGenesis(ctx sdk.Context, gs types.GenesisState) {
+	// Set the feemarket module's parameters.
+	if err := k.SetParams(ctx, gs.Params); err != nil {
+		panic(err)
+	}
+
+	// set the fee market implementation
+	if err := k.plugin.Unmarshal(gs.Plugin); err != nil {
+		panic(err)
+	}
+
+	if err := k.Init(ctx); err != nil {
+		panic(err)
+	}
+}
+
+// ExportGenesis returns a GenesisState for a given context.
+func (k *Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
+	// Get the feemarket module's parameters.
+	params, err := k.GetParams(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	return types.NewGenesisState(k.plugin, params)
+}
---
+++ x/feemarket/keeper/genesis_test.go
@@ -0,0 +1,15 @@
+package keeper_test
+
+func (s *KeeperTestSuite) TestInitGenesis() {
+	s.Run("valid genesis should not panic", func() {
+
+	})
+
+	s.Run("invalid params should panic", func() {
+
+	})
+
+	s.Run("invalid plugin bytes should panic", func() {
+
+	})
+}
