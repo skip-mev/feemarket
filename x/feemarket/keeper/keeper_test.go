56afdb303298f617e52db48359a74724e8eec741
aljo242 <alex@ingenuity.build>
2023-11-10 11:15:42 -0500

    fix test

--- x/feemarket/keeper/keeper_test.go
+++ x/feemarket/keeper/keeper_test.go
@@ -1,7 +1,6 @@
 package keeper_test
 
 import (
-	"github.com/skip-mev/feemarket/x/feemarket/keeper"
 	"testing"
 
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
@@ -9,12 +8,16 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/stretchr/testify/suite"
+
+	"github.com/skip-mev/feemarket/testutils"
+	"github.com/skip-mev/feemarket/x/feemarket/keeper"
+	"github.com/skip-mev/feemarket/x/feemarket/types"
 )
 
 type KeeperTestSuite struct {
 	suite.Suite
 
-	auctionkeeper    keeper.Keeper
+	feemarketKeeper  keeper.Keeper
 	encCfg           testutils.EncodingConfig
 	ctx              sdk.Context
 	key              *storetypes.KVStoreKey
@@ -31,25 +34,13 @@ func (s *KeeperTestSuite) SetupTest() {
 	testCtx := testutil.DefaultContextWithDB(s.T(), s.key, storetypes.NewTransientStoreKey("transient_test"))
 	s.ctx = testCtx.Ctx
 
-	s.accountKeeper = mocks.NewAccountKeeper(s.T())
-	s.accountKeeper.On("GetModuleAddress", types.ModuleName).Return(sdk.AccAddress{}).Maybe()
-
-	s.bankKeeper = mocks.NewBankKeeper(s.T())
-	s.distrKeeper = mocks.NewDistributionKeeper(s.T())
-	s.stakingKeeper = mocks.NewStakingKeeper(s.T())
-	s.authorityAccount = sdk.AccAddress([]byte("authority"))
-	s.auctionkeeper = keeper.NewKeeper(
+	s.authorityAccount = []byte("authority")
+	s.feemarketKeeper = keeper.NewKeeper(
 		s.encCfg.Codec,
 		s.key,
-		s.accountKeeper,
-		s.bankKeeper,
-		s.distrKeeper,
-		s.stakingKeeper,
 		s.authorityAccount.String(),
 	)
 
-	err := s.auctionkeeper.SetParams(s.ctx, types.DefaultParams())
+	err := s.feemarketKeeper.SetParams(s.ctx, types.DefaultParams())
 	s.Require().NoError(err)
-
-	s.msgServer = keeper.NewMsgServerImpl(s.auctionkeeper)
 }
