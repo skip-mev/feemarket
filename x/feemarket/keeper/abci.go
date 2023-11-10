f9409670ac89f6a88985267601973d52f088e2e7
aljo242 <alex@ingenuity.build>
2023-11-10 17:56:47 -0500

    module

--- x/feemarket/keeper/abci.go
+++ x/feemarket/keeper/abci.go
@@ -1,15 +1,27 @@
 package keeper
 
 import (
-	cometabci "github.com/cometbft/cometbft/abci/types"
+	abci "github.com/cometbft/cometbft/abci/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// BeginBlock returns an beginblocker for the x/feemarket module.
-func (k *Keeper) BeginBlock(ctx sdk.Context) ([]cometabci.ValidatorUpdate, error) {
+// BeginBlock returns a beginblocker for the x/feemarket module.
+func (k *Keeper) BeginBlock(ctx sdk.Context) ([]abci.ValidatorUpdate, error) {
 	handler := k.plugin.BeginBlockUpdateHandler(ctx)
 
 	err := handler(ctx)
 
-	return []cometabci.ValidatorUpdate{}, err
+	return []abci.ValidatorUpdate{}, err
+}
+
+// EndBlock returns an endblocker for the x/feemarket module.
+func (k *Keeper) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+	handler := k.plugin.EndBlockUpdateHandler(ctx)
+
+	err := handler(ctx)
+	if err != nil {
+		k.Logger(ctx).Error("error in end block", "error", err)
+	}
+
+	return []abci.ValidatorUpdate{}
 }
