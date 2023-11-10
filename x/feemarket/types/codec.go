10f90edc922535bd52b3eadc7d0fca5cf23abcd7
Alex Johnson <alex@skip.money>
2023-11-10 11:24:35 -0500

    chore: register fee market interfaces in codec (#8)
    * add interface
    
    * mock
    
    * md
    
    * md
    
    * md
    
    * lint fix
    
    * module
    
    * add proto
    
    * fmt
    
    * move files
    
    * proto-cute
    
    * nit fix
    
    * params
    
    * init
    
    * format
    
    * fix
    
    * clean
    
    * Update proto/feemarket/feemarket/v1/mock.proto
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>
    
    * Update x/feemarket/types/genesis.go
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>
    
    * regen
    
    * fix
    
    * fix
    
    * simplify
    
    * simplify
    
    * simplify
    
    * create file
    
    * ppopulate
    
    * register interfaces
    
    ---------
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>
    Co-authored-by: aljo242 <alex@ingenuity.build>

--- x/feemarket/types/codec.go
+++ x/feemarket/types/codec.go
@@ -6,12 +6,18 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+
+	"github.com/skip-mev/feemarket/x/feemarket/interfaces"
+	"github.com/skip-mev/feemarket/x/feemarket/plugins/defaultmarket"
 )
 
 // RegisterLegacyAminoCodec registers the necessary x/feemarket interfaces (messages) on the
 // provided LegacyAmino codec.
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	legacy.RegisterAminoMsg(cdc, &MsgParams{}, "feemarket/MsgParams")
+
+	cdc.RegisterInterface((*interfaces.FeeMarketImplementation)(nil), nil)
+	cdc.RegisterConcrete(&defaultmarket.DefaultMarket{}, "feemarket/DefaultMarket", nil)
 }
 
 // RegisterInterfaces registers the x/feemarket interfaces (messages + msg server) on the
@@ -19,6 +25,12 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 func RegisterInterfaces(registry types.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgParams{},
+	)
+
+	registry.RegisterInterface(
+		"feemarket.feemarket.v1.FeeMarketImplementation",
+		(*interfaces.FeeMarketImplementation)(nil),
+		&defaultmarket.DefaultMarket{},
 	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
