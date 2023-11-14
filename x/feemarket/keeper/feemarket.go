06d80444dc8c727104dd40cb0dbfcc2d1e9ea9e1
aljo242 <alex@skip.money>
2023-11-14 15:22:02 -0500

    fix

--- x/feemarket/keeper/feemarket.go
+++ x/feemarket/keeper/feemarket.go
@@ -6,22 +6,17 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// UpdateHandler is responsible for updating the parameters of the
-// fee market plugin.
-// Fees can optionally also be extracted here.
-type UpdateHandler func(ctx sdk.Context) error
-
 // ------------------- Fee Market Updates ------------------- //
 
 // Init initializes the fee market (in InitGenesis).
-func (k *Keeper) Init(ctx sdk.Context) error {
+func (k *Keeper) Init(_ sdk.Context) error {
 	// TODO initialize fee market state with params
 
 	return nil
 }
 
 // Export exports the fee market (in ExportGenesis).
-func (k *Keeper) Export(ctx sdk.Context) (json.RawMessage, error) {
+func (k *Keeper) Export(_ sdk.Context) (json.RawMessage, error) {
 	// TODO export state from fee market state
 
 	return nil, nil
@@ -29,7 +24,7 @@ func (k *Keeper) Export(ctx sdk.Context) (json.RawMessage, error) {
 
 // BeginBlockUpdateHandler allows the fee market to be updated
 // after every block. This will be added to the BeginBlock chain.
-func (k *Keeper) BeginBlockUpdateHandler(ctx sdk.Context) UpdateHandler {
+func (k *Keeper) BeginBlockUpdateHandler(_ sdk.Context) func(ctx sdk.Context) error {
 	return func(ctx sdk.Context) error {
 		return nil // TODO return handler
 	}
@@ -37,7 +32,7 @@ func (k *Keeper) BeginBlockUpdateHandler(ctx sdk.Context) UpdateHandler {
 
 // EndBlockUpdateHandler allows the fee market to be updated
 // after every block. This will be added to the EndBlock chain.
-func (k *Keeper) EndBlockUpdateHandler(ctx sdk.Context) UpdateHandler {
+func (k *Keeper) EndBlockUpdateHandler(_ sdk.Context) func(ctx sdk.Context) error {
 	return func(ctx sdk.Context) error {
 		return nil // TODO return handler
 	}
