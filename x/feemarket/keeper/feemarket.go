06bf9055d7c373262173672492597677e6f54f8d
David Terpay <35130517+davidterpay@users.noreply.github.com>
2023-11-15 11:01:59 -0500

    feat(state): Adding Helper Functions to state type (#22)
    * yait
    
    * lint
    
    * adding helpers for updating util
    
    * more helpers
    
    * end blocker init (#23)
    
    Co-authored-by: Alex Johnson <alex@skip.money>
    
    * test fix
    
    * lul
    
    ---------
    
    Co-authored-by: Alex Johnson <alex@skip.money>

--- x/feemarket/keeper/feemarket.go
+++ x/feemarket/keeper/feemarket.go
@@ -22,22 +22,52 @@ func (k *Keeper) Export(_ sdk.Context) (json.RawMessage, error) {
 	return nil, nil
 }
 
-// BeginBlockUpdateHandler allows the fee market to be updated
+// BeginBlocker allows the fee market to be updated
 // after every block. This will be added to the BeginBlock chain.
-func (k *Keeper) BeginBlockUpdateHandler(_ sdk.Context) func(ctx sdk.Context) error {
-	return func(ctx sdk.Context) error {
-		return nil // TODO return handler
-	}
+func (k *Keeper) BeginBlocker(_ sdk.Context) error {
+	return nil
 }
 
-// EndBlockUpdateHandler allows the fee market to be updated
+// EndBlocker allows the fee market to be updated
 // after every block. This will be added to the EndBlock chain.
-func (k *Keeper) EndBlockUpdateHandler(_ sdk.Context) func(ctx sdk.Context) error {
-	return func(ctx sdk.Context) error {
-		return nil // TODO return handler
+func (k *Keeper) EndBlocker(ctx sdk.Context) error {
+	params, err := k.GetParams(ctx)
+	if err != nil {
+		return err
 	}
+
+	if !params.Enabled {
+		return nil
+	}
+
+	state, err := k.GetState(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Update the learning rate based on the block utilization seen in the
+	// current block. This is the AIMD learning rate adjustment algorithm.
+	newLR := state.UpdateLearningRate(
+		params.Theta,
+		params.Alpha,
+		params.Beta,
+		params.MinLearningRate,
+		params.MaxLearningRate,
+	)
+
+	// Update the base fee based with the new learning rate and delta adjustment.
+	newBaseFee := state.UpdateBaseFee(params.Delta)
+
+	k.Logger(ctx).Info(
+		"updated the fee market",
+		"height", ctx.BlockHeight(),
+		"new_base_fee", newBaseFee,
+		"new_learning_rate", newLR,
+		"average_block_utilization", state.GetAverageUtilization(),
+		"net_block_utilization", state.GetNetUtilization(),
+	)
+
+	// Increment the height of the state and set the new state.
+	state.IncrementHeight()
+	return k.SetState(ctx, state)
 }
-
-// ------------------- Fee Market Queries ------------------- //
-
-// TODO add fee market state query
