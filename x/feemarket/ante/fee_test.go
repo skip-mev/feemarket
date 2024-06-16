88d007127dc6eb4b1d05ffa61225640597dc970e
Alex Johnson <alex@skip.money>
2024-06-16 11:46:31 -0400

    fix: don't fail post handler on simulate tx with no fee (#122)
    * ok
    
    * ok
    
    * format
    
    * ok
    
    * clean readme
    
    * retract
    
    * mod bump
    
    (cherry picked from commit 9a2a3eeed1e8329d95f248eace808b10707127e9)
    
    # Conflicts:
    #	go.mod
    #	go.sum
    #	x/feemarket/post/fee.go

--- x/feemarket/ante/fee_test.go
+++ x/feemarket/ante/fee_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"cosmossdk.io/math"
-
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -77,7 +76,7 @@ func TestAnteHandle(t *testing.T) {
 			ExpErr:   sdkerrors.ErrOutOfGas,
 		},
 		{
-			Name: "0 gas given should pass in simulate",
+			Name: "0 gas given should pass in simulate - no fee",
 			Malleate: func(suite *antesuite.TestSuite) antesuite.TestCaseArgs {
 				accs := suite.CreateTestAccounts(1)
 
@@ -85,6 +84,23 @@ func TestAnteHandle(t *testing.T) {
 					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
 					GasLimit:  0,
 					FeeAmount: nil,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  false,
+			Simulate: true,
+			ExpPass:  true,
+			ExpErr:   nil,
+		},
+		{
+			Name: "0 gas given should pass in simulate - fee",
+			Malleate: func(suite *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := suite.CreateTestAccounts(1)
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  0,
+					FeeAmount: validFee,
 				}
 			},
 			RunAnte:  true,
@@ -124,6 +140,40 @@ func TestAnteHandle(t *testing.T) {
 			Simulate: false,
 			ExpPass:  true,
 			ExpErr:   nil,
+		},
+		{
+			Name: "no fee - fail",
+			Malleate: func(s *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := s.CreateTestAccounts(1)
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  1000000000,
+					FeeAmount: nil,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  true,
+			Simulate: false,
+			ExpPass:  false,
+			ExpErr:   types.ErrNoFeeCoins,
+		},
+		{
+			Name: "no gas limit - fail",
+			Malleate: func(s *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := s.CreateTestAccounts(1)
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  0,
+					FeeAmount: nil,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  true,
+			Simulate: false,
+			ExpPass:  false,
+			ExpErr:   sdkerrors.ErrInvalidGasLimit,
 		},
 	}
 
--- x/feemarket/post/fee.go
+++ x/feemarket/post/fee.go
@@ -1,7 +1,13 @@
 package post
 
 import (
+<<<<<<< HEAD
+=======
+	"bytes"
+>>>>>>> 9a2a3ee (fix: don't fail post handler on simulate tx with no fee (#122))
 	"fmt"
+
+	"cosmossdk.io/math"
 
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/math"
@@ -73,14 +79,20 @@ func (dfd FeeMarketDeductDecorator) PostHandle(ctx sdk.Context, tx sdk.Tx, simul
 	feeCoins := feeTx.GetFee()
 	gas := ctx.GasMeter().GasConsumed() // use context gas consumed
 
-	if len(feeCoins) != 1 {
-		if len(feeCoins) == 0 {
-			return ctx, errorsmod.Wrapf(feemarkettypes.ErrNoFeeCoins, "got length %d", len(feeCoins))
-		}
+	if len(feeCoins) == 0 && !simulate {
+		return ctx, errorsmod.Wrapf(feemarkettypes.ErrNoFeeCoins, "got length %d", len(feeCoins))
+	}
+	if len(feeCoins) > 1 {
 		return ctx, errorsmod.Wrapf(feemarkettypes.ErrTooManyFeeCoins, "got length %d", len(feeCoins))
 	}
 
-	feeCoin := feeCoins[0]
+	var feeCoin sdk.Coin
+	if simulate && len(feeCoins) == 0 {
+		feeCoin = sdk.NewCoin(params.FeeDenom, math.ZeroInt())
+	} else {
+		feeCoin = feeCoins[0]
+	}
+
 	feeGas := int64(feeTx.GetGas())
 
 	var (
