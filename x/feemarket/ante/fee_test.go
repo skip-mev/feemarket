d1f22bd6564c96c97e6a17bfb643b36aa6b7493e
aljo242 <alex@skip.money>
2024-06-16 06:47:26 -0400

    ok

--- x/feemarket/ante/fee_test.go
+++ x/feemarket/ante/fee_test.go
@@ -5,12 +5,10 @@ import (
 	"testing"
 
 	"cosmossdk.io/math"
-
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	_ "github.com/cosmos/cosmos-sdk/x/auth"
-
 	antesuite "github.com/skip-mev/feemarket/x/feemarket/ante/suite"
 	"github.com/skip-mev/feemarket/x/feemarket/types"
 )
@@ -77,7 +75,7 @@ func TestAnteHandle(t *testing.T) {
 			ExpErr:   sdkerrors.ErrOutOfGas,
 		},
 		{
-			Name: "0 gas given should pass in simulate",
+			Name: "0 gas given should pass in simulate - no fee",
 			Malleate: func(suite *antesuite.TestSuite) antesuite.TestCaseArgs {
 				accs := suite.CreateTestAccounts(1)
 
@@ -85,6 +83,23 @@ func TestAnteHandle(t *testing.T) {
 					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
 					GasLimit:  0,
 					FeeAmount: nil,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  false,
+			Simulate: true,
+			ExpPass:  true,
+			ExpErr:   nil,
+		},
+		{
+			Name: "0 gas given should pass in simulate - fee",
+			Malleate: func(suite *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := suite.CreateTestAccounts(1)
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  0,
+					FeeAmount: validFee,
 				}
 			},
 			RunAnte:  true,
@@ -124,6 +139,40 @@ func TestAnteHandle(t *testing.T) {
 			Simulate: false,
 			ExpPass:  true,
 			ExpErr:   nil,
+		},
+		{
+			Name: "no fee - fail",
+			Malleate: func(s *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := s.CreateTestAccounts(1)
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  1000000000,
+					FeeAmount: nil,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  true,
+			Simulate: false,
+			ExpPass:  false,
+			ExpErr:   types.ErrNoFeeCoins,
+		},
+		{
+			Name: "no gas limit - fail",
+			Malleate: func(s *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := s.CreateTestAccounts(1)
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  0,
+					FeeAmount: nil,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  true,
+			Simulate: false,
+			ExpPass:  false,
+			ExpErr:   sdkerrors.ErrInvalidGasLimit,
 		},
 	}
 
--- x/feemarket/post/fee_test.go
+++ x/feemarket/post/fee_test.go
@@ -344,11 +344,60 @@ func TestPostHandle(t *testing.T) {
 			ExpErr:   nil,
 		},
 		{
-			Name: "no fee - simulate",
+			Name: "0 gas given should pass in simulate - no fee",
+			Malleate: func(suite *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := suite.CreateTestAccounts(1)
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  0,
+					FeeAmount: nil,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  false,
+			Simulate: true,
+			ExpPass:  true,
+			ExpErr:   nil,
+		},
+		{
+			Name: "0 gas given should pass in simulate - fee",
+			Malleate: func(suite *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := suite.CreateTestAccounts(1)
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  0,
+					FeeAmount: validFee,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  false,
+			Simulate: true,
+			ExpPass:  true,
+			ExpErr:   nil,
+		},
+		{
+			Name: "no fee - fail",
 			Malleate: func(s *antesuite.TestSuite) antesuite.TestCaseArgs {
 				accs := s.CreateTestAccounts(1)
-				s.MockBankKeeper.On("SendCoinsFromAccountToModule", mock.Anything, accs[0].Account.GetAddress(), types.FeeCollectorName, mock.Anything).Return(nil)
-				s.MockBankKeeper.On("SendCoins", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Once()
+
+				return antesuite.TestCaseArgs{
+					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
+					GasLimit:  1000000000,
+					FeeAmount: nil,
+				}
+			},
+			RunAnte:  true,
+			RunPost:  true,
+			Simulate: false,
+			ExpPass:  false,
+			ExpErr:   types.ErrNoFeeCoins,
+		},
+		{
+			Name: "no gas limit - fail",
+			Malleate: func(s *antesuite.TestSuite) antesuite.TestCaseArgs {
+				accs := s.CreateTestAccounts(1)
 
 				return antesuite.TestCaseArgs{
 					Msgs:      []sdk.Msg{testdata.NewTestMsg(accs[0].Account.GetAddress())},
@@ -358,9 +407,9 @@ func TestPostHandle(t *testing.T) {
 			},
 			RunAnte:  true,
 			RunPost:  true,
-			Simulate: true,
-			ExpPass:  true,
-			ExpErr:   nil,
+			Simulate: false,
+			ExpPass:  false,
+			ExpErr:   sdkerrors.ErrInvalidGasLimit,
 		},
 	}
 
